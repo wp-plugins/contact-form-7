cc5fab80fbe19feee8cb9305385a694dda7cd49d
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-08-23 10:07:13 +0000

    wpautop_substitute
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@17311 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-contact-form-7.php
+++ wp-contact-form-7.php
@@ -38,6 +38,44 @@ class tam_contact_form_seven {
 		add_action('wp_print_scripts', array(&$this, 'load_js'));
 		add_action('init', array(&$this, 'ajax_json_echo'));
 		add_filter('the_content', array(&$this, 'the_content_filter'), 9);
+		remove_filter('the_content', 'wpautop');
+		add_filter('the_content', array(&$this, 'wpautop_substitute'));
+	}
+	
+	// Original wpautop function has harmful effect on formatting of form elements.
+	// This wpautop_substitute is a temporary substitution until original is patched.
+	// See http://trac.wordpress.org/ticket/4605
+	function wpautop_substitute($pee, $br = 1) {
+		$pee = $pee . "\n"; // just to make things a little easier, pad the end
+		$pee = preg_replace('|<br />\s*<br />|', "\n\n", $pee);
+		// Space things out a little
+		$allblocks = '(?:address|area|blockquote|caption|colgroup|dd|div|dl|dt|form|h[1-6]|li|map|math|ol|p|pre|table|tbody|td|tfoot|th|thead|tr|ul)';
+		$pee = preg_replace('!(<' . $allblocks . '[^>]*>)!', "\n$1", $pee);
+		$pee = preg_replace('!(</' . $allblocks . '>)!', "$1\n\n", $pee);
+		$pee = str_replace(array("\r\n", "\r"), "\n", $pee); // cross-platform newlines
+		$pee = preg_replace("/\n\n+/", "\n\n", $pee); // take care of duplicates
+		$pee = preg_replace('/\n?(.+?)(?:\n\s*\n|\z)/s', "<p>$1</p>\n", $pee); // make paragraphs, including one at the end
+		$pee = preg_replace('|<p>\s*?</p>|', '', $pee); // under certain strange conditions it could create a P of entirely whitespace
+		$pee = preg_replace('!<p>([^<]+)\s*?(</(?:div|address|form)[^>]*>)!', "<p>$1</p>$2", $pee);
+		$pee = preg_replace( '|<p>|', "$1<p>", $pee );
+		$pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee); // don't pee all over a tag
+		$pee = preg_replace("|<p>(<li.+?)</p>|", "$1", $pee); // problem with nested lists
+		$pee = preg_replace('|<p><blockquote([^>]*)>|i', "<blockquote$1><p>", $pee);
+		$pee = str_replace('</blockquote></p>', '</p></blockquote>', $pee);
+		$pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)!', "$1", $pee);
+		$pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee);
+		if ($br) {
+			$pee = preg_replace('/<(script|style).*?<\/\\1>/se', 'str_replace("\n", "<WPPreserveNewline />", "\\0")', $pee);
+			$pee = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $pee); // optionally make line breaks
+			$pee = str_replace('<WPPreserveNewline />', "\n", $pee);
+		}
+		$pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*<br />!', "$1", $pee);
+		$pee = preg_replace('!<br />(\s*</?(?:p|li|div|dl|dd|dt|th|pre|td|ul|ol)[^>]*>)!', '$1', $pee);
+		if (strpos($pee, '<pre') !== false)
+			$pee = preg_replace('!(<pre.*?>)(.*?)</pre>!ise', " stripslashes('$1') .  stripslashes(clean_pre('$2'))  . '</pre>' ", $pee);
+		$pee = preg_replace( "|\n</p>$|", '</p>', $pee );
+		
+		return $pee;
 	}
 	
 	function ajax_json_echo() {
