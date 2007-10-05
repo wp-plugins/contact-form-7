9593c4172b3eb8b616598c00122927cd2b7242a4
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-05 08:35:49 +0000

    Exclude javascript.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@20926 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ contact-form-7.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function() {
+	jQuery('div.wpcf7 > form').ajaxForm({
+		beforeSubmit: beforeSubmit,
+		url: _wpcf7.ajaxUrl,
+		dataType: 'json',
+		success: processJson
+	});
+});
+
+function beforeSubmit(formData, jqForm, options) {
+	clearResponseOutput();
+	jQuery('img.ajax-loader', jqForm[0]).css({ visibility: 'visible' });
+	return true;
+}
+
+function notValidTip(input, message) {
+	jQuery(input).after('<span class="wpcf7-not-valid-tip">' + message + '</span>');
+	jQuery('span.wpcf7-not-valid-tip').mouseover(function() {
+		jQuery(this).fadeOut('fast');
+	});
+	jQuery(input).mouseover(function() {
+		jQuery(input).siblings('.wpcf7-not-valid-tip').not(':hidden').fadeOut('fast');
+	});
+	jQuery(input).focus(function() {
+		jQuery(input).siblings('.wpcf7-not-valid-tip').not(':hidden').fadeOut('fast');
+	});
+}
+
+function processJson(data) {
+	var wpcf7ResponseOutput = jQuery(data.into).find('div.wpcf7-response-output');
+	clearResponseOutput();
+	if (data.invalids) {
+		jQuery.each(data.invalids, function(i, n) {
+			notValidTip(jQuery(data.into).find(n.into), n.message);
+		});
+		wpcf7ResponseOutput.addClass('wpcf7-validation-errors');
+	}
+	if (data.captcha) {
+		jQuery.each(data.captcha, function(i, n) {
+			jQuery(data.into).find(':input[@name="' + i + '"]').clearFields();
+			jQuery(data.into).find('img.wpcf7-captcha-' + i).attr('src', n);
+			var match = /([0-9]+)\.(png|gif|jpeg)$/.exec(n);
+			jQuery(data.into).find('input:hidden[@name="_wpcf7_captcha_challenge_' + i + '"]').attr('value', match[1]);
+		});
+	}
+	if (1 == data.spam) {
+		wpcf7ResponseOutput.addClass('wpcf7-spam-blocked');
+	}
+	if (1 == data.mailSent) {
+		jQuery(data.into).find('form').resetForm().clearForm();
+		wpcf7ResponseOutput.addClass('wpcf7-mail-sent-ok');
+	} else {
+		wpcf7ResponseOutput.addClass('wpcf7-mail-sent-ng');
+	}
+	wpcf7ResponseOutput.append(data.message).fadeIn('fast');
+}
+
+function clearResponseOutput() {
+	jQuery('div.wpcf7-response-output').hide().empty().removeClass('wpcf7-mail-sent-ok wpcf7-mail-sent-ng wpcf7-validation-errors wpcf7-spam-blocked');
+	jQuery('span.wpcf7-not-valid-tip').remove();
+	jQuery('img.ajax-loader').css({ visibility: 'hidden' });
+}
--- wp-contact-form-7.php
+++ wp-contact-form-7.php
@@ -501,6 +501,8 @@ class tam_contact_form_seven {
 		$stylesheet_url = get_option('siteurl') . '/wp-content/plugins/contact-form-7/stylesheet.css';
 		echo '<link rel="stylesheet" href="' . $stylesheet_url . '" type="text/css" />';
 		
+		$javascript_url = get_option('siteurl') . '/wp-content/plugins/contact-form-7/contact-form-7.js';
+		
 		$url = parse_url($_SERVER['REQUEST_URI']);
 		if (empty($url['query']))
 			$override_url = $url['path'] . '?wpcf7=json';
@@ -509,73 +511,9 @@ class tam_contact_form_seven {
 		
 ?>
 <script type="text/javascript">
-//<![CDATA[
-
-jQuery(document).ready(function() {
-	jQuery('div.wpcf7 > form').ajaxForm({
-		beforeSubmit: beforeSubmit,
-		url: '<?php echo $override_url; ?>',
-		dataType: 'json',
-		success: processJson
-	});
-});
-
-function beforeSubmit(formData, jqForm, options) {
-	clearResponseOutput();
-	jQuery('img.ajax-loader', jqForm[0]).css({ visibility: 'visible' });
-	return true;
-}
-
-function notValidTip(input, message) {
-	jQuery(input).after('<span class="wpcf7-not-valid-tip">' + message + '</span>');
-	jQuery('span.wpcf7-not-valid-tip').mouseover(function() {
-		jQuery(this).fadeOut('fast');
-	});
-	jQuery(input).mouseover(function() {
-		jQuery(input).siblings('.wpcf7-not-valid-tip').not(':hidden').fadeOut('fast');
-	});
-	jQuery(input).focus(function() {
-		jQuery(input).siblings('.wpcf7-not-valid-tip').not(':hidden').fadeOut('fast');
-	});
-}
-
-function processJson(data) {
-	var wpcf7ResponseOutput = jQuery(data.into).find('div.wpcf7-response-output');
-	clearResponseOutput();
-	if (data.invalids) {
-		jQuery.each(data.invalids, function(i, n) {
-			notValidTip(jQuery(data.into).find(n.into), n.message);
-		});
-		wpcf7ResponseOutput.addClass('wpcf7-validation-errors');
-	}
-	if (data.captcha) {
-		jQuery.each(data.captcha, function(i, n) {
-			jQuery(data.into).find(':input[@name="' + i + '"]').clearFields();
-			jQuery(data.into).find('img.wpcf7-captcha-' + i).attr('src', n);
-			var match = /([0-9]+)\.(png|gif|jpeg)$/.exec(n);
-			jQuery(data.into).find('input:hidden[@name="_wpcf7_captcha_challenge_' + i + '"]').attr('value', match[1]);
-		});
-	}
-	if (1 == data.spam) {
-		wpcf7ResponseOutput.addClass('wpcf7-spam-blocked');
-	}
-	if (1 == data.mailSent) {
-		jQuery(data.into).find('form').resetForm().clearForm();
-		wpcf7ResponseOutput.addClass('wpcf7-mail-sent-ok');
-	} else {
-		wpcf7ResponseOutput.addClass('wpcf7-mail-sent-ng');
-	}
-	wpcf7ResponseOutput.append(data.message).fadeIn('fast');
-}
-
-function clearResponseOutput() {
-	jQuery('div.wpcf7-response-output').hide().empty().removeClass('wpcf7-mail-sent-ok wpcf7-mail-sent-ng wpcf7-validation-errors wpcf7-spam-blocked');
-	jQuery('span.wpcf7-not-valid-tip').remove();
-	jQuery('img.ajax-loader').css({ visibility: 'hidden' });
-}
-
-//]]>
+var _wpcf7 = { ajaxUrl: '<?php echo $override_url; ?>' };
 </script>
+<script type='text/javascript' src='<?php echo $javascript_url; ?>'></script>
 <?php
 	}
 	
