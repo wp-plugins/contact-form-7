451ae362b6a7cab6f3817f55639c5d34af60952a
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-26 08:04:41 +0000

    Importing panel for a meantime.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@70505 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin-panel-import.php
@@ -0,0 +1,7 @@
+<?php if (isset($updated_message)) : ?>
+<div id="message" class="updated fade"><p><strong><?php echo $updated_message; ?></strong></p></div>
+<?php endif; ?>
+<div class="wrap">
+	<h2><?php _e('Contact Form 7', 'wpcf7'); ?></h2>
+
+</div>
