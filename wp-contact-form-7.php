b24794611541ed3936f159bda6eb6a04e66fe8bd
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-07 12:43:33 +0000

    Copyright year updated
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@92571 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-contact-form-7.php
+++ wp-contact-form-7.php
@@ -8,7 +8,7 @@ Version: 1.9.2.2
 Author URI: http://ideasilo.wordpress.com/
 */
 
-/*  Copyright 2007-2008 Takayuki Miyoshi (email: takayukister at gmail.com)
+/*  Copyright 2007-2009 Takayuki Miyoshi (email: takayukister at gmail.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
