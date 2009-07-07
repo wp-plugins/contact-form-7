a6d6d7164d8392263280e337d4bb7dcea066c78a
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-07 13:13:45 +0000

    Modify shortcode code for Contact Form 7 fashion
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@132676 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -4,6 +4,9 @@ class WPCF7_ShortcodeManager {
 
 	var $shortcode_tags = array();
 
+	// Taggs scanned at the last time of do_shortcode()
+	var $scanned_tags = null;
+
 	function add_shortcode( $tag, $func ) {
 		if ( is_callable( $func ) )
 			$this->shortcode_tags[$tag] = $func;
@@ -13,11 +16,9 @@ class WPCF7_ShortcodeManager {
 		unset( $this->shortcode_tags[$tag] );
 	}
 
-	function remove_all_shortcodes() {
-		$this->shortcode_tags = array();
-	}
-
 	function do_shortcode( $content ) {
+		$this->scanned_tags = array();
+
 		if ( empty( $this->shortcode_tags ) || ! is_array( $this->shortcode_tags) )
 			return $content;
 
@@ -30,7 +31,7 @@ class WPCF7_ShortcodeManager {
 		$tagnames = array_keys( $this->shortcode_tags );
 		$tagregexp = join( '|', array_map( 'preg_quote', $tagnames ) );
 
-		return '(.?)\[(' . $tagregexp . ')\b(.*?)(?:(\/))?\](?:(.+?)\[\/\2\])?(.?)';
+		return '(.?)\[(' . $tagregexp . ')(?:\s(.*?))?(?:(\/))?\](?:(.+?)\[\/\2\])?(.?)';
 	}
 
 	function do_shortcode_tag( $m ) {
@@ -41,6 +42,11 @@ class WPCF7_ShortcodeManager {
 
 		$tag = $m[2];
 		$attr = $this->shortcode_parse_atts( $m[3] );
+
+		$this->scanned_tags[] = array(
+			'type' => $tag,
+			'attr' => $attr,
+			'content' => $m[5] );
 
 		if ( isset( $m[5] ) ) {
 			// enclosing tag - extra parameter
@@ -53,47 +59,26 @@ class WPCF7_ShortcodeManager {
 
 	function shortcode_parse_atts( $text ) {
 		$atts = array();
-		$pattern = '/(\w+)\s*=\s*"([^"]*)"(?:\s|$)|(\w+)\s*=\s*\'([^\']*)\'(?:\s|$)|(\w+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|(\S+)(?:\s|$)/';
 		$text = preg_replace( "/[\x{00a0}\x{200b}]+/u", " ", $text );
-		if ( preg_match_all( $pattern, $text, $match, PREG_SET_ORDER ) ) {
-			foreach ( $match as $m ) {
-				if ( ! empty( $m[1] ) )
-					$atts[strtolower( $m[1] )] = stripcslashes( $m[2] );
-				elseif ( ! empty( $m[3] ) )
-					$atts[strtolower( $m[3] )] = stripcslashes( $m[4] );
-				elseif ( ! empty($m[5] ) )
-					$atts[strtolower( $m[5] )] = stripcslashes( $m[6] );
-				elseif ( isset( $m[7] ) and strlen( $m[7] ) )
-					$atts[] = stripcslashes( $m[7] );
-				elseif ( isset( $m[8] ) )
-					$atts[] = stripcslashes( $m[8] );
+		$text = trim( $text );
+
+		$pattern = '%^([-0-9a-zA-Z:.#_/|\s]*?)?((?:\s+(?:"[^"]*"|\'[^\']*\'))*)?$%';
+
+		if ( preg_match( $pattern, $text, $match ) ) {
+			if ( ! empty( $match[1] ) ) {
+				$atts['options'] = preg_split( '/[\s]+/', trim( $match[1] ) );
+				if ( ! empty( $atts['options'] ) )
+					$atts['maybe_name'] = $atts['options'][0];
+			}
+			if ( ! empty( $match[2] ) ) {
+				preg_match_all( '/"[^"]*"|\'[^\']*\'/', $match[2], $matched_values );
+				$atts['values'] = wpcf7_strip_quote_deep( $matched_values[0] );
 			}
 		} else {
-			$atts = ltrim( $text );
+			$atts = $text;
 		}
+
 		return $atts;
-	}
-
-	function shortcode_atts( $pairs, $atts ) {
-		$atts = (array) $atts;
-		$out = array();
-
-		foreach( $pairs as $name => $default ) {
-			if ( array_key_exists( $name, $atts ) )
-				$out[$name] = $atts[$name];
-			else
-				$out[$name] = $default;
-		}
-		return $out;
-	}
-
-	function strip_shortcodes( $content ) {
-		if ( empty( $this->shortcode_tags ) || ! is_array( $this->shortcode_tags ) )
-			return $content;
-
-		$pattern = $this->get_shortcode_regex();
-
-		return preg_replace( '/' . $pattern . '/s', '', $content );
 	}
 
 }
@@ -116,6 +101,12 @@ function wpcf7_do_shortcode( $content ) {
 	global $wpcf7_shortcode_manager;
 
 	return $wpcf7_shortcode_manager->do_shortcode( $content );
+}
+
+function wpcf7_scanned_shortcodes() {
+	global $wpcf7_shortcode_manager;
+
+	return $wpcf7_shortcode_manager->scanned_tags;
 }
 
 ?>
