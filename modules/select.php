01cd52b5f06e95a837168b38eeb1cba632de2f8f
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-10 05:54:05 +0000

    Refactoring
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@133709 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/select.php
+++ modules/select.php
@@ -14,53 +14,59 @@ function wpcf7_select_shortcode_handler( $tag ) {
 	$options = (array) $tag['options'];
 	$values = (array) $tag['values'];
 
-	$validation_error = '';
-	if ( is_a( $wpcf7_contact_form, 'WPCF7_ContactForm' ) )
-		$validation_error = $wpcf7_contact_form->validation_error( $name );
-
 	$atts = '';
+	$id_att = '';
+	$class_att = '';
 
-	$id_array = preg_grep( '%^id:[-0-9a-zA-Z_]+$%', $options );
-	if ( $id = array_shift( $id_array ) ) {
-		preg_match( '%^id:([-0-9a-zA-Z_]+)$%', $id, $id_matches );
-		if ( $id = $id_matches[1] )
-			$atts .= ' id="' . $id . '"';
-	}
+	$defaults = array();
 
-	$class_att = "";
-	$class_array = preg_grep( '%^class:[-0-9a-zA-Z_]+$%', $options );
-	foreach ( $class_array as $class ) {
-		preg_match( '%^class:([-0-9a-zA-Z_]+)$%', $class, $class_matches );
-		if ( $class = $class_matches[1] )
-			$class_att .= ' ' . $class;
-	}
-
-	if ( preg_match( '/[*]$/', $type ) )
+	if ( 'select*' == $type )
 		$class_att .= ' wpcf7-validates-as-required';
+
+	foreach ( $options as $option ) {
+		if ( preg_match( '%^id:([-0-9a-zA-Z_]+)$%', $option, $matches ) ) {
+			$id_att = $matches[1];
+
+		} elseif ( preg_match( '%^class:([-0-9a-zA-Z_]+)$%', $option, $matches ) ) {
+			$class_att .= ' ' . $matches[1];
+
+		} elseif ( preg_match( '/^default:([0-9_]+)$/', $option, $matches ) ) {
+			$defaults = explode( '_', $matches[1] );
+		}
+	}
+
+	if ( $id_att )
+		$atts .= ' id="' . trim( $id_att ) . '"';
 
 	if ( $class_att )
 		$atts .= ' class="' . trim( $class_att ) . '"';
 
-	$scr_defaults = array_values( preg_grep( '/^default:/', $options ) );
-	preg_match( '/^default:([0-9_]+)$/', $scr_defaults[0], $scr_default_matches );
-	$scr_default = explode( '_', $scr_default_matches[1] );
-
-	$multiple = ( preg_grep( '%^multiple$%', $options ) ) ? true : false;
-	$include_blank = preg_grep( '%^include_blank$%', $options );
+	$multiple = (bool) preg_grep( '%^multiple$%', $options );
+	$include_blank = (bool) preg_grep( '%^include_blank$%', $options );
 
 	$empty_select = empty( $values );
 	if ( $empty_select || $include_blank )
 		array_unshift( $values, '---' );
 
 	$html = '';
+
+	$posted = is_a( $wpcf7_contact_form, 'WPCF7_ContactForm' ) && $wpcf7_contact_form->is_posted();
+
 	foreach ( $values as $key => $value ) {
-		$selected = '';
-		if ( ! $empty_select && in_array( $key + 1, (array) $scr_default ) )
-			$selected = ' selected="selected"';
-		if ( is_object( $wpcf7_contact_form ) && $wpcf7_contact_form->is_posted() && (
-				$multiple && in_array( $wpdb->escape( $value ), (array) $_POST[$name] ) ||
-				! $multiple && $_POST[$name] == $wpdb->escape( $value ) ) )
-			$selected = ' selected="selected"';
+		$selected = false;
+
+		if ( ! $empty_select && in_array( $key + 1, (array) $defaults ) )
+			$selected = true;
+
+		if ( $posted ) {
+			if ( $multiple && in_array( $wpdb->escape( $value ), (array) $_POST[$name] ) )
+				$selected = true;
+			if ( ! $multiple && $_POST[$name] == $wpdb->escape( $value ) )
+				$selected = true;
+		}
+
+		$selected = $selected ? ' selected="selected"' : '';
+
 		$html .= '<option value="' . esc_attr( $value ) . '"' . $selected . '>' . esc_html( $value ) . '</option>';
 	}
 
@@ -68,6 +74,11 @@ function wpcf7_select_shortcode_handler( $tag ) {
 		$atts .= ' multiple="multiple"';
 
 	$html = '<select name="' . $name . ( $multiple ? '[]' : '' ) . '"' . $atts . '>' . $html . '</select>';
+
+	$validation_error = '';
+	if ( is_a( $wpcf7_contact_form, 'WPCF7_ContactForm' ) )
+		$validation_error = $wpcf7_contact_form->validation_error( $name );
+
 	$html = '<span class="wpcf7-form-control-wrap ' . $name . '">' . $html . $validation_error . '</span>';
 
 	return $html;
