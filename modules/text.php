1416c7f5d186c7dde1a9d97d87e10b4facafd430
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-10 03:01:11 +0000

    Refactoring
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@133668 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/text.php
+++ modules/text.php
@@ -14,38 +14,47 @@ function wpcf7_text_shortcode_handler( $tag ) {
 	$options = (array) $tag['options'];
 	$values = (array) $tag['values'];
 
-	$validation_error = '';
-	if ( is_a( $wpcf7_contact_form, 'WPCF7_ContactForm' ) )
-		$validation_error = $wpcf7_contact_form->validation_error( $name );
-
 	$atts = '';
+	$id_att = '';
+	$class_att = '';
+	$size_att = '';
+	$maxlength_att = '';
 
-	$id_array = preg_grep( '%^id:[-0-9a-zA-Z_]+$%', $options );
-	if ( $id = array_shift( $id_array ) ) {
-		preg_match( '%^id:([-0-9a-zA-Z_]+)$%', $id, $id_matches );
-		if ( $id = $id_matches[1] )
-			$atts .= ' id="' . $id . '"';
-	}
-
-	$class_att = "";
-	$class_array = preg_grep( '%^class:[-0-9a-zA-Z_]+$%', $options );
-	foreach ( $class_array as $class ) {
-		preg_match( '%^class:([-0-9a-zA-Z_]+)$%', $class, $class_matches );
-		if ( $class = $class_matches[1] )
-			$class_att .= ' ' . $class;
-	}
-
-	if ( preg_match( '/^email[*]?$/', $type ) )
+	if ( 'email' == $type || 'email*' == $type )
 		$class_att .= ' wpcf7-validates-as-email';
 
-	if ( preg_match( '/[*]$/', $type ) )
+	if ( 'text*' == $type || 'email*' == $type )
 		$class_att .= ' wpcf7-validates-as-required';
+
+	foreach ( $options as $option ) {
+		if ( preg_match( '%^id:([-0-9a-zA-Z_]+)$%', $option, $matches ) ) {
+			$id_att = $matches[1];
+
+		} elseif ( preg_match( '%^class:([-0-9a-zA-Z_]+)$%', $option, $matches ) ) {
+			$class_att .= ' ' . $matches[1];
+
+		} elseif ( preg_match( '%^([0-9]*)[/x]([0-9]*)$%', $option, $matches ) ) {
+			$size_att = (int) $matches[1];
+			$maxlength_att = (int) $matches[2];
+		}
+	}
+
+	if ( $id_att )
+		$atts .= ' id="' . trim( $id_att ) . '"';
 
 	if ( $class_att )
 		$atts .= ' class="' . trim( $class_att ) . '"';
 
+	if ( $size_att )
+		$atts .= ' size="' . $size_att . '"';
+	else
+		$atts .= ' size="40"'; // default size
+
+	if ( $maxlength_att )
+		$atts .= ' maxlength="' . $maxlength_att . '"';
+
 	// Value
-	if ( is_object( $wpcf7_contact_form ) && $wpcf7_contact_form->is_posted() ) {
+	if ( is_a( $wpcf7_contact_form, 'WPCF7_ContactForm' ) && $wpcf7_contact_form->is_posted() ) {
 		if ( isset( $_POST['_wpcf7_mail_sent'] ) && $_POST['_wpcf7_mail_sent']['ok'] )
 			$value = '';
 		else
@@ -54,20 +63,12 @@ function wpcf7_text_shortcode_handler( $tag ) {
 		$value = $values[0];
 	}
 
-	$size_maxlength_array = preg_grep( '%^[0-9]*[/x][0-9]*$%', $options );
-	if ( $size_maxlength = array_shift( $size_maxlength_array ) ) {
-		preg_match( '%^([0-9]*)[/x]([0-9]*)$%', $size_maxlength, $sm_matches );
-		if ( $size = (int) $sm_matches[1] )
-			$atts .= ' size="' . $size . '"';
-		else
-			$atts .= ' size="40"';
-		if ( $maxlength = (int) $sm_matches[2] )
-			$atts .= ' maxlength="' . $maxlength . '"';
-	} else {
-		$atts .= ' size="40"';
-	}
-
 	$html = '<input type="text" name="' . $name . '" value="' . esc_attr( $value ) . '"' . $atts . ' />';
+
+	$validation_error = '';
+	if ( is_a( $wpcf7_contact_form, 'WPCF7_ContactForm' ) )
+		$validation_error = $wpcf7_contact_form->validation_error( $name );
+
 	$html = '<span class="wpcf7-form-control-wrap ' . $name . '">' . $html . $validation_error . '</span>';
 
 	return $html;
--- modules/textarea.php
+++ modules/textarea.php
@@ -14,35 +14,46 @@ function wpcf7_textarea_shortcode_handler( $tag ) {
 	$options = (array) $tag['options'];
 	$values = (array) $tag['values'];
 
-	$validation_error = '';
-	if ( is_a( $wpcf7_contact_form, 'WPCF7_ContactForm' ) )
-		$validation_error = $wpcf7_contact_form->validation_error( $name );
-
 	$atts = '';
+	$id_att = '';
+	$class_att = '';
+	$cols_att = '';
+	$rows_att = '';
 
-	$id_array = preg_grep( '%^id:[-0-9a-zA-Z_]+$%', $options );
-	if ( $id = array_shift( $id_array ) ) {
-		preg_match( '%^id:([-0-9a-zA-Z_]+)$%', $id, $id_matches );
-		if ( $id = $id_matches[1] )
-			$atts .= ' id="' . $id . '"';
-	}
-
-	$class_att = "";
-	$class_array = preg_grep( '%^class:[-0-9a-zA-Z_]+$%', $options );
-	foreach ( $class_array as $class ) {
-		preg_match( '%^class:([-0-9a-zA-Z_]+)$%', $class, $class_matches );
-		if ( $class = $class_matches[1] )
-			$class_att .= ' ' . $class;
-	}
-
-	if ( preg_match( '/[*]$/', $type ) )
+	if ( 'textarea*' == $type )
 		$class_att .= ' wpcf7-validates-as-required';
+
+	foreach ( $options as $option ) {
+		if ( preg_match( '%^id:([-0-9a-zA-Z_]+)$%', $option, $matches ) ) {
+			$id_att = $matches[1];
+
+		} elseif ( preg_match( '%^class:([-0-9a-zA-Z_]+)$%', $option, $matches ) ) {
+			$class_att .= ' ' . $matches[1];
+
+		} elseif ( preg_match( '%^([0-9]*)[x/]([0-9]*)$%', $option, $matches ) ) {
+			$cols_att = (int) $matches[1];
+			$rows_att = (int) $matches[2];
+		}
+	}
+
+	if ( $id_att )
+		$atts .= ' id="' . trim( $id_att ) . '"';
 
 	if ( $class_att )
 		$atts .= ' class="' . trim( $class_att ) . '"';
 
+	if ( $cols_att )
+		$atts .= ' cols="' . $cols_att . '"';
+	else
+		$atts .= ' cols="40"'; // default size
+
+	if ( $rows_att )
+		$atts .= ' rows="' . $rows_att . '"';
+	else
+		$atts .= ' rows="10"'; // default size
+
 	// Value
-	if ( is_object( $wpcf7_contact_form ) && $wpcf7_contact_form->is_posted() ) {
+	if ( is_a( $wpcf7_contact_form, 'WPCF7_ContactForm' ) && $wpcf7_contact_form->is_posted() ) {
 		if ( isset( $_POST['_wpcf7_mail_sent'] ) && $_POST['_wpcf7_mail_sent']['ok'] )
 			$value = '';
 		else
@@ -51,22 +62,12 @@ function wpcf7_textarea_shortcode_handler( $tag ) {
 		$value = $values[0];
 	}
 
-	$cols_rows_array = preg_grep( '%^[0-9]*[x/][0-9]*$%', $options );
-	if ( $cols_rows = array_shift( $cols_rows_array ) ) {
-		preg_match( '%^([0-9]*)[x/]([0-9]*)$%', $cols_rows, $cr_matches );
-		if ( $cols = (int) $cr_matches[1] )
-			$atts .= ' cols="' . $cols . '"';
-		else
-			$atts .= ' cols="40"';
-		if ( $rows = (int) $cr_matches[2] )
-			$atts .= ' rows="' . $rows . '"';
-		else
-			$atts .= ' rows="10"';
-	} else {
-		$atts .= ' cols="40" rows="10"';
-	}
-
 	$html = '<textarea name="' . $name . '"' . $atts . '>' . esc_html( $value ) . '</textarea>';
+
+	$validation_error = '';
+	if ( is_a( $wpcf7_contact_form, 'WPCF7_ContactForm' ) )
+		$validation_error = $wpcf7_contact_form->validation_error( $name );
+
 	$html = '<span class="wpcf7-form-control-wrap ' . $name . '">' . $html . $validation_error . '</span>';
 
 	return $html;
