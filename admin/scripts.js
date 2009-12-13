714fd88cb86482634fa76dba9320bdb70942f52e
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-13 03:30:07 +0000

    whitespace cleanup
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@182243 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/scripts.js
+++ admin/scripts.js
@@ -1,105 +1,108 @@
 jQuery(document).ready(function() {
-    try {
-        jQuery('input#wpcf7-title:enabled').css({
-            cursor: 'pointer'
-        });
+	try {
+		jQuery('input#wpcf7-title:enabled').css({
+			cursor: 'pointer'
+		});
 
-        jQuery('input#wpcf7-title').mouseover(function() {
-            jQuery(this).not('.focus').css({
-                'background-color': '#ffffdd'
-            });
-        });
+		jQuery('input#wpcf7-title').mouseover(function() {
+			jQuery(this).not('.focus').css({
+				'background-color': '#ffffdd'
+			});
+		});
 
-        jQuery('input#wpcf7-title').mouseout(function() {
-            jQuery(this).css({
-                'background-color': '#fff'
-            });
-        });
+		jQuery('input#wpcf7-title').mouseout(function() {
+			jQuery(this).css({
+				'background-color': '#fff'
+			});
+		});
 
-        jQuery('input#wpcf7-title').focus(function() {
-            jQuery(this).addClass('focus');
-            jQuery(this).css({
-                cursor: 'text',
-                color: '#333',
-                border: '1px solid #777',
-                font: 'normal 13px Verdana, Arial, Helvetica, sans-serif',
-                'background-color': '#fff'
-            });
-        });
+		jQuery('input#wpcf7-title').focus(function() {
+			jQuery(this).addClass('focus');
+			jQuery(this).css({
+				cursor: 'text',
+				color: '#333',
+				border: '1px solid #777',
+				font: 'normal 13px Verdana, Arial, Helvetica, sans-serif',
+				'background-color': '#fff'
+			});
+		});
 
-        jQuery('input#wpcf7-title').blur(function() {
-            jQuery(this).removeClass('focus');
-            jQuery(this).css({
-                cursor: 'pointer',
-                color: '#555',
-                border: 'none',
-                font: 'bold 20px serif',
-                'background-color': '#fff'
-            });
-        });
+		jQuery('input#wpcf7-title').blur(function() {
+			jQuery(this).removeClass('focus');
+			jQuery(this).css({
+				cursor: 'pointer',
+				color: '#555',
+				border: 'none',
+				font: 'bold 20px serif',
+				'background-color': '#fff'
+			});
+		});
 
-        jQuery('input#wpcf7-title').change(function() {
-            updateTag();
-        });
+		jQuery('input#wpcf7-title').change(function() {
+			updateTag();
+		});
 
-        updateTag();
+		updateTag();
 
-        if (! jQuery('#wpcf7-mail-2-active').is(':checked'))
-            jQuery('#mail-2-fields').hide();
+		if (! jQuery('#wpcf7-mail-2-active').is(':checked'))
+			jQuery('#mail-2-fields').hide();
 
-        jQuery('#wpcf7-mail-2-active').click(function() {
-            if (jQuery('#wpcf7-mail-2-active').is(':checked')) {
-                if (jQuery('#mail-2-fields').is(':hidden'))
-                    jQuery('#mail-2-fields').slideDown('fast');
-            } else {
-                if (jQuery('#mail-2-fields').is(':visible'))
-                    jQuery('#mail-2-fields').hide('fast');
-            }
-        });
+		jQuery('#wpcf7-mail-2-active').click(function() {
+			if (jQuery('#wpcf7-mail-2-active').is(':checked')) {
+				if (jQuery('#mail-2-fields').is(':hidden'))
+					jQuery('#mail-2-fields').slideDown('fast');
+			} else {
+				if (jQuery('#mail-2-fields').is(':visible'))
+					jQuery('#mail-2-fields').hide('fast');
+			}
+		});
 
-        jQuery('#message-fields-toggle-switch').text(_wpcf7L10n.show);
-        jQuery('#message-fields').hide();
+		jQuery('#message-fields-toggle-switch').text(_wpcf7L10n.show);
+		jQuery('#message-fields').hide();
 
-        jQuery('#message-fields-toggle-switch').click(function() {
-            if (jQuery('#message-fields').is(':hidden')) {
-                jQuery('#message-fields').slideDown('fast');
-                jQuery('#message-fields-toggle-switch').text(_wpcf7L10n.hide);
-            } else {
-                jQuery('#message-fields').hide('fast');
-                jQuery('#message-fields-toggle-switch').text(_wpcf7L10n.show);
-            }
-        });
+		jQuery('#message-fields-toggle-switch').click(function() {
+			if (jQuery('#message-fields').is(':hidden')) {
+				jQuery('#message-fields').slideDown('fast');
+				jQuery('#message-fields-toggle-switch').text(_wpcf7L10n.hide);
+			} else {
+				jQuery('#message-fields').hide('fast');
+				jQuery('#message-fields-toggle-switch').text(_wpcf7L10n.show);
+			}
+		});
 
-        if ('' == jQuery.trim(jQuery('#wpcf7-additional-settings').text())) {
-            jQuery('#additional-settings-fields-toggle-switch').text(_wpcf7L10n.show);
-            jQuery('#additional-settings-fields').hide();
-        } else {
-            jQuery('#additional-settings-fields-toggle-switch').text(_wpcf7L10n.hide);
-            jQuery('#additional-settings-fields').show();
-        }
+		if ('' == jQuery.trim(jQuery('#wpcf7-additional-settings').text())) {
+			jQuery('#additional-settings-fields-toggle-switch').text(_wpcf7L10n.show);
+			jQuery('#additional-settings-fields').hide();
+		} else {
+			jQuery('#additional-settings-fields-toggle-switch').text(_wpcf7L10n.hide);
+			jQuery('#additional-settings-fields').show();
+		}
 
-        jQuery('#additional-settings-fields-toggle-switch').click(function() {
-            if (jQuery('#additional-settings-fields').is(':hidden')) {
-                jQuery('#additional-settings-fields').slideDown('fast');
-                jQuery('#additional-settings-fields-toggle-switch').text(_wpcf7L10n.hide);
-            } else {
-                jQuery('#additional-settings-fields').hide('fast');
-                jQuery('#additional-settings-fields-toggle-switch').text(_wpcf7L10n.show);
-            }
-        });
+		jQuery('#additional-settings-fields-toggle-switch').click(function() {
+			if (jQuery('#additional-settings-fields').is(':hidden')) {
+				jQuery('#additional-settings-fields').slideDown('fast');
+				jQuery('#additional-settings-fields-toggle-switch').text(_wpcf7L10n.hide);
+			} else {
+				jQuery('#additional-settings-fields').hide('fast');
+				jQuery('#additional-settings-fields-toggle-switch').text(_wpcf7L10n.show);
+			}
+		});
 
-    } catch (e) {
-    }
+	} catch (e) {
+	}
 });
 
 function updateTag() {
-  var title = jQuery('input#wpcf7-title').val();
-  if (title)
-    title = title.replace(/["'\[\]]/g, '');
-  jQuery('input#wpcf7-title').val(title);
-  var current = jQuery('input#wpcf7-id').val();
-  var tag = '[contact-form ' + current + ' "' + title + '"]';
-  jQuery('input#contact-form-anchor-text').val(tag);
+	var title = jQuery('input#wpcf7-title').val();
+
+	if (title)
+		title = title.replace(/["'\[\]]/g, '');
+
+	jQuery('input#wpcf7-title').val(title);
+	var current = jQuery('input#wpcf7-id').val();
+	var tag = '[contact-form ' + current + ' "' + title + '"]';
+
+	jQuery('input#contact-form-anchor-text').val(tag);
 }
 
 
@@ -165,8 +168,8 @@ jQuery.extend(jQuery.tgPanes, {
 });
 
 jQuery(document).ready(function() {
-    try {
+	try {
 		jQuery('#taggenerator').tagGenerator();
-    } catch (e) {
-    }
+	} catch (e) {
+	}
 });
