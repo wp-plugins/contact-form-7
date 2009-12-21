526fb0a8b12e8035bb18b47b45fe4be7cf9b89d2
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-21 06:00:38 +0000

    Don't use CF7's functions
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@185542 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -6,12 +6,35 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 function wpcf7_delete_plugin() {
 	global $wpdb;
 
+	/* Delete tables */
+
 	$table_name = $wpdb->prefix . "contact_form_7";
 
 	$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
 
-	wpcf7_rmdir( wpcf7_captcha_tmp_dir() );
-	wpcf7_rmdir( wpcf7_upload_tmp_dir() );
+	/* Delete directories */
+
+	$upload_path = trim( get_option( 'upload_path' ) );
+
+	if ( empty( $upload_path ) )
+		$dir = WP_CONTENT_DIR . '/uploads';
+	else
+		$dir = $upload_path;
+
+	$dir = path_join( ABSPATH, $dir );
+
+	if ( defined( 'UPLOADS' ) )
+		$dir = ABSPATH . UPLOADS;
+
+	$captcha_tmp_dir = defined( 'WPCF7_CAPTCHA_TMP_DIR' )
+		? WPCF7_CAPTCHA_TMP_DIR : $dir . '/wpcf7_captcha';
+
+	wpcf7_rmdir( $captcha_tmp_dir );
+
+	$file_tmp_dir = defined( 'WPCF7_UPLOADS_TMP_DIR' )
+		? WPCF7_UPLOADS_TMP_DIR : $dir . '/wpcf7_uploads';
+
+	wpcf7_rmdir( $file_tmp_dir );
 }
 
 function wpcf7_rmdir( $dir ) {
