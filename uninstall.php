a3c1736871c8459b6328f056bec3cd26d3d6e41d
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-22 03:30:19 +0000

    delete outdated wpcf7 option
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@196493 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -6,6 +6,8 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 function wpcf7_delete_plugin() {
 	global $wpdb;
 
+	delete_option( 'wpcf7' );
+
 	$table_name = $wpdb->prefix . "contact_form_7";
 
 	$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
