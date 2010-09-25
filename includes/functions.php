42c2e61b3aa65134db63e130a92fb760abf5b526
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-25 10:02:31 +0000

    Malay language translation created by Zairul Azmil
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@293433 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/functions.php
+++ includes/functions.php
@@ -197,6 +197,7 @@ function wpcf7_l10n() {
 		'ko_KR' => __( 'Korean', 'wpcf7' ),
 		'lv' => __( 'Latvian', 'wpcf7' ),
 		'lt_LT' => __( 'Lithuanian', 'wpcf7' ),
+		'ms_MY' => __( 'Malay', 'wpcf7' ),
 		'ml_IN' => __( 'Malayalam', 'wpcf7' ),
 		'nb_NO' => __( 'Norwegian', 'wpcf7' ),
 		'fa_IR' => __( 'Persian', 'wpcf7' ),
