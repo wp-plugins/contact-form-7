f2c5444a2169997beb55a0c8b15f0be54287c494
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-06 12:10:15 +0000

    Delete all wpcf7_contact_form posts when deleting the plugin.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@433947 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,14 @@ function wpcf7_delete_plugin() {
 
 	delete_option( 'wpcf7' );
 
+	$posts = get_posts( array(
+		'numberposts' => -1,
+		'post_type' => 'wpcf7_contact_form',
+		'post_status' => 'any' ) );
+
+	foreach ( $posts as $post )
+		wp_delete_post( $post->ID, true );
+
 	$table_name = $wpdb->prefix . "contact_form_7";
 
 	$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
