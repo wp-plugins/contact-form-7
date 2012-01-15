b21f9827a838f69fba70e9a60e73bfa26326c036
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-15 13:43:34 +0000

    Menu icon created by Takao Honda.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@490213 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/admin.php
+++ admin/admin.php
@@ -112,7 +112,8 @@ add_action( 'admin_menu', 'wpcf7_admin_menu', 9 );
 
 function wpcf7_admin_menu() {
 	add_menu_page( __( 'Contact Form 7', 'wpcf7' ), __( 'Contact', 'wpcf7' ),
-		WPCF7_ADMIN_READ_CAPABILITY, 'wpcf7', 'wpcf7_admin_management_page' );
+		WPCF7_ADMIN_READ_CAPABILITY, 'wpcf7', 'wpcf7_admin_management_page',
+		wpcf7_plugin_url( 'admin/images/menu-icon.png' ) );
 
 	add_submenu_page( 'wpcf7', __( 'Edit Contact Forms', 'wpcf7' ), __( 'Edit', 'wpcf7' ),
 		WPCF7_ADMIN_READ_CAPABILITY, 'wpcf7', 'wpcf7_admin_management_page' );
