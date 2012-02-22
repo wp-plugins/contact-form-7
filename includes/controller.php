808d2ee2849a31f7b1d559634c92de0c119d0fb7
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-22 06:56:10 +0000

    Update jquery.form.js to 2.96
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@508724 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/controller.php
+++ includes/controller.php
@@ -231,7 +231,7 @@ function wpcf7_enqueue_scripts() {
 	// so we need to deregister it and re-register the latest one
 	wp_deregister_script( 'jquery-form' );
 	wp_register_script( 'jquery-form', wpcf7_plugin_url( 'jquery.form.js' ),
-		array( 'jquery' ), '2.95', true );
+		array( 'jquery' ), '2.96', true );
 
 	$in_footer = true;
 	if ( 'header' === WPCF7_LOAD_JS )
