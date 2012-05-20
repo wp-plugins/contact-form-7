6336e24817022074d7722640ebf202d55ec2179b
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-20 05:26:59 +0000

    Show Jetpack notice only on Contact Form 7's admin pages.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@546447 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/jetpack.php
+++ modules/jetpack.php
@@ -3,15 +3,20 @@
 add_action( 'admin_notices', 'wpcf7_jetpack_admin_notices' );
 
 function wpcf7_jetpack_admin_notices() {
+	$current_screen = get_current_screen();
+
+	if ( false === strpos( $current_screen->id, 'wpcf7' ) )
+		return;
+
 	if ( ! class_exists( 'Jetpack' )
 	|| ! Jetpack::is_module( 'contact-form' )
 	|| ! in_array( 'contact-form', Jetpack::get_active_modules() ) )
 		return;
 
-	$url = 'http://contactform7.com/jetpack-overrides-contact-forms';
+	$url = 'http://contactform7.com/jetpack-overrides-contact-forms/';
 ?>
 <div class="error">
-<p><?php echo sprintf( __( '<strong>Jetpack causes trouble to other plugins in certain cases.</strong> <a href="%s" target="_blank">See how you can avoid it.</a>', 'wpcf7' ), $url ); ?></p>
+<p><?php echo sprintf( __( '<strong>Jetpack may cause problems for other plugins in certain cases.</strong> <a href="%s" target="_blank">See how to avoid it.</a>', 'wpcf7' ), $url ); ?></p>
 </div>
 <?php
 }
