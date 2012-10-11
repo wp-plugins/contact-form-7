3f89dd63a99742a42308289e5fad7344d3a93b19
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-11 03:36:32 +0000

    Avoid possible XSS. $_SERVER['HTTP_USER_AGENT'] is not reliable.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@610599 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/special-mail-tags.php
+++ modules/special-mail-tags.php
@@ -3,9 +3,9 @@
 ** Filters for Special Mail Tags
 **/
 
-add_filter( 'wpcf7_special_mail_tags', 'wpcf7_special_mail_tag', 10, 2 );
+add_filter( 'wpcf7_special_mail_tags', 'wpcf7_special_mail_tag', 10, 3 );
 
-function wpcf7_special_mail_tag( $output, $name ) {
+function wpcf7_special_mail_tag( $output, $name, $html ) {
 
 	// For backwards compat.
 	$name = preg_replace( '/^wpcf7\./', '_', $name );
@@ -13,8 +13,12 @@ function wpcf7_special_mail_tag( $output, $name ) {
 	if ( '_remote_ip' == $name )
 		$output = preg_replace( '/[^0-9a-f.:, ]/', '', $_SERVER['REMOTE_ADDR'] );
 
-	elseif ( '_user_agent' == $name )
+	elseif ( '_user_agent' == $name ) {
 		$output = substr( $_SERVER['HTTP_USER_AGENT'], 0, 254 );
+
+		if ( $html )
+			$output = esc_html( $output );
+	}
 
 	elseif ( '_url' == $name ) {
 		$url = untrailingslashit( home_url() );
