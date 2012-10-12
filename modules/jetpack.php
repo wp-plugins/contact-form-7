36a356c205f4a8f60d6574a1f2cfe2fe878318e9
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-12 16:11:18 +0000

    It's not necessary to show Jetpack notice to users who didn't use to use older versions than 3.0.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@611574 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/jetpack.php
+++ modules/jetpack.php
@@ -3,9 +3,16 @@
 add_action( 'wpcf7_admin_notices', 'wpcf7_jetpack_admin_notices' );
 
 function wpcf7_jetpack_admin_notices() {
+	global $wpdb;
+
 	if ( ! class_exists( 'Jetpack' )
 	|| ! Jetpack::is_module( 'contact-form' )
 	|| ! in_array( 'contact-form', Jetpack::get_active_modules() ) )
+		return;
+
+	$q = "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_old_cf7_unit_id'";
+
+	if ( ! $wpdb->get_var( $q ) )
 		return;
 
 	$url = 'http://contactform7.com/jetpack-overrides-contact-forms/';
