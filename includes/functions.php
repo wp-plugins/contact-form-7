c700fc0e7bf48657ef7bf3477763bac28b7ef20d
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-20 12:54:57 +0000

    Simply use wp_upload_dir() instead of copying long code. Ref: http://plugins.trac.wordpress.org/changeset/655467/contact-form-7
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@684768 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/functions.php
+++ includes/functions.php
@@ -135,47 +135,11 @@ function wpcf7_default_messages_template() {
 }
 
 function wpcf7_upload_dir( $type = false ) {
-	global $switched;
+	$uploads = wp_upload_dir();
 
-	$siteurl = get_option( 'siteurl' );
-	$upload_path = trim( get_option( 'upload_path' ) );
-
-	$main_override = is_multisite() && defined( 'MULTISITE' ) && is_main_site();
-
-	if ( empty( $upload_path ) ) {
-		$dir = WP_CONTENT_DIR . '/uploads';
-	} else {
-		$dir = $upload_path;
-
-		if ( 'wp-content/uploads' == $upload_path ) {
-			$dir = WP_CONTENT_DIR . '/uploads';
-		} elseif ( 0 !== strpos( $dir, ABSPATH ) ) {
-			// $dir is absolute, $upload_path is (maybe) relative to ABSPATH
-			$dir = path_join( ABSPATH, $dir );
-		}
-	}
-
-	if ( ! $url = get_option( 'upload_url_path' ) ) {
-		if ( empty( $upload_path )
-		|| ( 'wp-content/uploads' == $upload_path )
-		|| ( $upload_path == $dir ) )
-			$url = WP_CONTENT_URL . '/uploads';
-		else
-			$url = trailingslashit( $siteurl ) . $upload_path;
-	}
-
-	if ( defined( 'UPLOADS' ) && ! $main_override
-	&& ( ! isset( $switched ) || $switched === false ) ) {
-		$dir = ABSPATH . UPLOADS;
-		$url = trailingslashit( $siteurl ) . UPLOADS;
-
-		if ( is_multisite() && defined( 'BLOGUPLOADDIR' ) ) {
-			$dir = untrailingslashit( BLOGUPLOADDIR );
-			$url = str_replace( UPLOADS, 'files', $url );
-		}
-	}
-
-	$uploads = apply_filters( 'wpcf7_upload_dir', array( 'dir' => $dir, 'url' => $url ) );
+	$uploads = apply_filters( 'wpcf7_upload_dir', array(
+		'dir' => $uploads['basedir'],
+		'url' => $uploads['baseurl'] ) );
 
 	if ( 'dir' == $type )
 		return $uploads['dir'];
