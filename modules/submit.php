6b83ed118c8866b410bc9621745821b603b3ce11
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-03 21:05:48 +0000

    Revise submit mod using WPCF7_Shortcode class.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@691383 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/submit.php
+++ modules/submit.php
@@ -8,43 +8,23 @@
 wpcf7_add_shortcode( 'submit', 'wpcf7_submit_shortcode_handler' );
 
 function wpcf7_submit_shortcode_handler( $tag ) {
-	if ( ! is_array( $tag ) )
-		return '';
+	$tag = new WPCF7_Shortcode( $tag );
 
-	$options = (array) $tag['options'];
-	$values = (array) $tag['values'];
+	$class = wpcf7_form_controls_class( $tag->type );
 
-	$atts = $id_att = $tabindex_att = '';
+	$atts = $tag->make_common_atts( array( 'class' => $class ) );
 
-	$class_att = wpcf7_form_controls_class( 'submit' );
+	$value = isset( $tag->values[0] ) ? $tag->values[0] : '';
 
-	foreach ( $options as $option ) {
-		if ( preg_match( '%^id:([-0-9a-zA-Z_]+)$%', $option, $matches ) ) {
-			$id_att = $matches[1];
-
-		} elseif ( preg_match( '%^class:([-0-9a-zA-Z_]+)$%', $option, $matches ) ) {
-			$class_att .= ' ' . $matches[1];
-
-		} elseif ( preg_match( '%^tabindex:(\d+)$%', $option, $matches ) ) {
-			$tabindex_att = (int) $matches[1];
-
-		}
-	}
-
-	if ( $id_att )
-		$atts .= ' id="' . trim( $id_att ) . '"';
-
-	if ( $class_att )
-		$atts .= ' class="' . trim( $class_att ) . '"';
-
-	if ( '' !== $tabindex_att )
-		$atts .= sprintf( ' tabindex="%d"', $tabindex_att );
-
-	$value = isset( $values[0] ) ? $values[0] : '';
 	if ( empty( $value ) )
 		$value = __( 'Send', 'wpcf7' );
 
-	$html = '<input type="submit" value="' . esc_attr( $value ) . '"' . $atts . ' />';
+	$atts['type'] = 'submit';
+	$atts['value'] = $value;
+
+	$atts = wpcf7_format_atts( $atts );
+
+	$html = sprintf( '<input %1$s />', $atts );
 
 	return $html;
 }
