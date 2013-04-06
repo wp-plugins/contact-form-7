73cde0f351ccb4503fd940763225666205dd7479
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-06 10:38:19 +0000

    Update jquery.form.js to the latest 3.32.0-2013.04.03
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@692654 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/controller.php
+++ includes/controller.php
@@ -228,7 +228,7 @@ function wpcf7_enqueue_scripts() {
 	wp_deregister_script( 'jquery-form' );
 	wp_register_script( 'jquery-form',
 		wpcf7_plugin_url( 'includes/js/jquery.form.min.js' ),
-		array( 'jquery' ), '3.25.0-2013.01.18', true );
+		array( 'jquery' ), '3.32.0-2013.04.03', true );
 
 	$in_footer = true;
 	if ( 'header' === WPCF7_LOAD_JS )
