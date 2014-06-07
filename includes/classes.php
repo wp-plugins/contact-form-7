df1319d4fb2eb1b284a52939fd5db601571db5d1
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-07 17:25:53 +0000

    Add lang and dir tats to top level <div>s.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@928251 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/classes.php
+++ includes/classes.php
@@ -210,7 +210,12 @@ class WPCF7_ContactForm {
 
 		$this->unit_tag = self::get_unit_tag( $this->id );
 
-		$html = '<div class="wpcf7" id="' . $this->unit_tag . '">' . "\n";
+		$html = sprintf( '<div %s>', wpcf7_format_atts( array(
+			'class' => 'wpcf7',
+			'id' => $this->unit_tag,
+			( get_option( 'html_type' ) == 'text/html' ) ? 'lang' : 'xml:lang'
+				=> str_replace( '_', '-', $this->locale ),
+			'dir' => wpcf7_is_rtl( $this->locale ) ? 'rtl' : '' ) ) ) . "\n";
 
 		$html .= $this->screen_reader_response() . "\n";
 
