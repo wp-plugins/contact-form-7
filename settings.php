5cf6cb8f80edf92e384becde1901db08e6bcd937
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-10 12:15:21 +0000

    Rename classes.php => contact-form.php
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@929588 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings.php
+++ settings.php
@@ -6,7 +6,7 @@ require_once WPCF7_PLUGIN_DIR . '/includes/formatting.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/pipe.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/shortcodes.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/capabilities.php';
-require_once WPCF7_PLUGIN_DIR . '/includes/classes.php';
+require_once WPCF7_PLUGIN_DIR . '/includes/contact-form.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/mail.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/submission.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/upgrade.php';
