d773beeb3519ebebecc15bdf4cb2f1aab505c915
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-15 06:41:46 +0000

    Move contact form related functions to contact-form.php.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@932533 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/contact-form.php
+++ includes/contact-form.php
@@ -764,4 +764,111 @@ function wpcf7_form_controls_class( $type, $default = '' ) {
 	return implode( ' ', $classes );
 }
 
+function wpcf7_get_default_template( $prop = 'form' ) {
+	if ( 'form' == $prop )
+		$template = wpcf7_default_form_template();
+	elseif ( 'mail' == $prop )
+		$template = wpcf7_default_mail_template();
+	elseif ( 'mail_2' == $prop )
+		$template = wpcf7_default_mail_2_template();
+	elseif ( 'messages' == $prop )
+		$template = wpcf7_default_messages_template();
+	else
+		$template = null;
+
+	return apply_filters( 'wpcf7_default_template', $template, $prop );
+}
+
+function wpcf7_default_form_template() {
+	$template =
+		'<p>' . __( 'Your Name', 'contact-form-7' ) . ' ' . __( '(required)', 'contact-form-7' ) . '<br />' . "\n"
+		. '    [text* your-name] </p>' . "\n\n"
+		. '<p>' . __( 'Your Email', 'contact-form-7' ) . ' ' . __( '(required)', 'contact-form-7' ) . '<br />' . "\n"
+		. '    [email* your-email] </p>' . "\n\n"
+		. '<p>' . __( 'Subject', 'contact-form-7' ) . '<br />' . "\n"
+		. '    [text your-subject] </p>' . "\n\n"
+		. '<p>' . __( 'Your Message', 'contact-form-7' ) . '<br />' . "\n"
+		. '    [textarea your-message] </p>' . "\n\n"
+		. '<p>[submit "' . __( 'Send', 'contact-form-7' ) . '"]</p>';
+
+	return $template;
+}
+
+function wpcf7_default_mail_template() {
+	$subject = '[your-subject]';
+	$sender = '[your-name] <[your-email]>';
+	$body = sprintf( __( 'From: %s', 'contact-form-7' ), '[your-name] <[your-email]>' ) . "\n"
+		. sprintf( __( 'Subject: %s', 'contact-form-7' ), '[your-subject]' ) . "\n\n"
+		. __( 'Message Body:', 'contact-form-7' ) . "\n" . '[your-message]' . "\n\n" . '--' . "\n"
+		. sprintf( __( 'This e-mail was sent from a contact form on %1$s (%2$s)', 'contact-form-7' ),
+			get_bloginfo( 'name' ), get_bloginfo( 'url' ) );
+	$recipient = get_option( 'admin_email' );
+	$additional_headers = '';
+	$attachments = '';
+	$use_html = 0;
+	$exclude_blank = 0;
+	return compact( 'subject', 'sender', 'body', 'recipient', 'additional_headers', 'attachments', 'use_html', 'exclude_blank' );
+}
+
+function wpcf7_default_mail_2_template() {
+	$active = false;
+	$subject = '[your-subject]';
+	$sender = '[your-name] <[your-email]>';
+	$body = __( 'Message Body:', 'contact-form-7' ) . "\n" . '[your-message]' . "\n\n" . '--' . "\n"
+		. sprintf( __( 'This e-mail was sent from a contact form on %1$s (%2$s)', 'contact-form-7' ),
+			get_bloginfo( 'name' ), get_bloginfo( 'url' ) );
+	$recipient = '[your-email]';
+	$additional_headers = '';
+	$attachments = '';
+	$use_html = 0;
+	$exclude_blank = 0;
+	return compact( 'active', 'subject', 'sender', 'body', 'recipient', 'additional_headers', 'attachments', 'use_html', 'exclude_blank' );
+}
+
+function wpcf7_default_messages_template() {
+	$messages = array();
+
+	foreach ( wpcf7_messages() as $key => $arr ) {
+		$messages[$key] = $arr['default'];
+	}
+
+	return $messages;
+}
+
+function wpcf7_messages() {
+	$messages = array(
+		'mail_sent_ok' => array(
+			'description' => __( "Sender's message was sent successfully", 'contact-form-7' ),
+			'default' => __( 'Your message was sent successfully. Thanks.', 'contact-form-7' )
+		),
+
+		'mail_sent_ng' => array(
+			'description' => __( "Sender's message was failed to send", 'contact-form-7' ),
+			'default' => __( 'Failed to send your message. Please try later or contact the administrator by another method.', 'contact-form-7' )
+		),
+
+		'validation_error' => array(
+			'description' => __( "Validation errors occurred", 'contact-form-7' ),
+			'default' => __( 'Validation errors occurred. Please confirm the fields and submit it again.', 'contact-form-7' )
+		),
+
+		'spam' => array(
+			'description' => __( "Submission was referred to as spam", 'contact-form-7' ),
+			'default' => __( 'Failed to send your message. Please try later or contact the administrator by another method.', 'contact-form-7' )
+		),
+
+		'accept_terms' => array(
+			'description' => __( "There are terms that the sender must accept", 'contact-form-7' ),
+			'default' => __( 'Please accept the terms to proceed.', 'contact-form-7' )
+		),
+
+		'invalid_required' => array(
+			'description' => __( "There is a field that the sender must fill in", 'contact-form-7' ),
+			'default' => __( 'Please fill the required field.', 'contact-form-7' )
+		)
+	);
+
+	return apply_filters( 'wpcf7_messages', $messages );
+}
+
 ?>
