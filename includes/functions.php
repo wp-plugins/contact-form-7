7a68570465d011019e2486e22d9747bb68b55aac
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-28 11:22:39 +0000

    Merge special-mail-tags mod into mail.php.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@940018 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/functions.php
+++ includes/functions.php
@@ -266,7 +266,7 @@ function wpcf7_load_modules() {
 	}
 
 	$mods = array(
-		'acceptance', 'flamingo', 'special-mail-tags',
+		'acceptance', 'flamingo',
 		'akismet', 'jetpack', 'submit', 'captcha', 'number',
 		'text', 'checkbox', 'quiz', 'textarea', 'date',
 		'response', 'file', 'select', 'listo' );
