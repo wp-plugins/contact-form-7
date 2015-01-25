def33e23b1e9c2c4ad000e0727290fd373e736ce
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-25 10:27:50 +0000

    Revise Akismet module: send all POST content to Akismet (not only form-tagged field values).
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@1075217 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/akismet.php
+++ modules/akismet.php
@@ -7,42 +7,38 @@
 add_filter( 'wpcf7_spam', 'wpcf7_akismet' );
 
 function wpcf7_akismet( $spam ) {
-	if ( $spam )
+	if ( $spam ) {
 		return $spam;
+	}
 
 	if ( ! wpcf7_akismet_is_available() ) {
 		return false;
 	}
 
-	if ( ! $params = wpcf7_akismet_submitted_params() )
+	if ( ! $params = wpcf7_akismet_submitted_params() ) {
 		return false;
+	}
 
 	$c = array();
 
-	if ( ! empty( $params['author'] ) )
-		$c['comment_author'] = $params['author'];
-
-	if ( ! empty( $params['author_email'] ) )
-		$c['comment_author_email'] = $params['author_email'];
-
-	if ( ! empty( $params['author_url'] ) )
-		$c['comment_author_url'] = $params['author_url'];
-
-	if ( ! empty( $params['content'] ) )
-		$c['comment_content'] = $params['content'];
+	$c['comment_author'] = $params['author'];
+	$c['comment_author_email'] = $params['author_email'];
+	$c['comment_author_url'] = $params['author_url'];
+	$c['comment_content'] = $params['content'];
 
 	$c['blog'] = get_option( 'home' );
 	$c['blog_lang'] = get_locale();
 	$c['blog_charset'] = get_option( 'blog_charset' );
-	$c['user_ip'] = preg_replace( '/[^0-9., ]/', '', $_SERVER['REMOTE_ADDR'] );
+	$c['user_ip'] = $_SERVER['REMOTE_ADDR'];
 	$c['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
 	$c['referrer'] = $_SERVER['HTTP_REFERER'];
 
 	// http://blog.akismet.com/2012/06/19/pro-tip-tell-us-your-comment_type/
 	$c['comment_type'] = 'contact-form';
 
-	if ( $permalink = get_permalink() )
+	if ( $permalink = get_permalink() ) {
 		$c['permalink'] = $permalink;
+	}
 
 	$ignore = array( 'HTTP_COOKIE', 'HTTP_COOKIE2', 'PHP_AUTH_PW' );
 
@@ -70,46 +66,52 @@ function wpcf7_akismet_submitted_params() {
 	$params = array(
 		'author' => '',
 		'author_email' => '',
-		'author_url' => '' );
+		'author_url' => '',
+		'content' => '' );
 
-	$content = '';
+	$has_akismet_option = false;
 
-	$fes = wpcf7_scan_shortcode();
-
-	foreach ( $fes as $fe ) {
-		if ( ! isset( $fe['name'] ) || ! isset( $_POST[$fe['name']] ) )
+	foreach ( (array) $_POST as $key => $val ) {
+		if ( '_wpcf7' == substr( $key, 0, 6 ) || '_wpnonce' == $key ) {
 			continue;
-
-		$value = $_POST[$fe['name']];
-
-		if ( is_array( $value ) )
-			$value = implode( ', ', wpcf7_array_flatten( $value ) );
-
-		$value = trim( $value );
-
-		$options = (array) $fe['options'];
-
-		if ( preg_grep( '%^akismet:author$%', $options ) ) {
-			$params['author'] = trim( $params['author'] . ' ' . $value );
-
-		} elseif ( preg_grep( '%^akismet:author_email$%', $options ) ) {
-			if ( '' == $params['author_email'] )
-				$params['author_email'] = $value;
-
-		} elseif ( preg_grep( '%^akismet:author_url$%', $options ) ) {
-			if ( '' == $params['author_url'] )
-				$params['author_url'] = $value;
 		}
 
-		$content = trim( $content . "\n\n" . $value );
+		if ( is_array( $val ) ) {
+			$val = implode( ', ', wpcf7_array_flatten( $val ) );
+		}
+
+		$val = trim( $val );
+
+		if ( 0 == strlen( $val ) ) {
+			continue;
+		}
+
+		if ( $tags = wpcf7_scan_shortcode( array( 'name' => $key ) ) ) {
+			$tag = $tags[0];
+			$tag = new WPCF7_Shortcode( $tag );
+
+			$akismet = $tag->get_option( 'akismet',
+				'(author|author_email|author_url)', true );
+
+			if ( $akismet ) {
+				$has_akismet_option = true;
+
+				if ( 'author' == $akismet ) {
+					$params[$akismet] = trim( $params[$akismet] . ' ' . $val );
+				} elseif ( '' == $params[$akismet] ) {
+					$params[$akismet] = $val;
+				}
+			}
+		}
+
+		$params['content'] .= "\n\n" . $val;
 	}
 
-	$params = array_filter( $params );
-
-	if ( ! $params )
+	if ( ! $has_akismet_option ) {
 		return false;
+	}
 
-	$params['content'] = $content;
+	$params['content'] = trim( $params['content'] );
 
 	return $params;
 }
