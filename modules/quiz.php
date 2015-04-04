71897d2e15f34fd1a6d2fdaaee47531145fdd84f
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-04 05:00:18 +0000

    Use <label></label> in [quiz] for accessibility. Props joedolson https://wordpress.org/support/topic/quiz-input-type-not-accessible
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@1127526 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/quiz.php
+++ modules/quiz.php
@@ -60,7 +60,7 @@ function wpcf7_quiz_shortcode_handler( $tag ) {
 	$atts = wpcf7_format_atts( $atts );
 
 	$html = sprintf(
-		'<span class="wpcf7-form-control-wrap %1$s"><span class="wpcf7-quiz-label">%2$s</span>&nbsp;<input %3$s /><input type="hidden" name="_wpcf7_quiz_answer_%4$s" value="%5$s" />%6$s</span>',
+		'<span class="wpcf7-form-control-wrap %1$s"><label><span class="wpcf7-quiz-label">%2$s</span> <input %3$s /></label><input type="hidden" name="_wpcf7_quiz_answer_%4$s" value="%5$s" />%6$s</span>',
 		sanitize_html_class( $tag->name ),
 		esc_html( $question ), $atts, $tag->name,
 		wp_hash( $answer, 'wpcf7_quiz' ), $validation_error );
