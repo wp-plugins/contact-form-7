06742b5b973f0f9d7198cc8935ce50574265b85d
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 12:14:09 +0000

    Edit screen: Revert the last change. Need to modify taggenerator.js first.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@1134466 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -20,7 +20,11 @@
 			});
 
 			$.extend($.tgPanes, _wpcf7.tagGenerators);
-			$('#taggenerator').tagGenerator(_wpcf7.generateTag);
+			$('#taggenerator').tagGenerator(_wpcf7.generateTag, {
+				dropdownIconUrl: _wpcf7.pluginUrl + '/admin/images/dropdown.gif',
+				fadebuttImageUrl: _wpcf7.pluginUrl + '/admin/images/fade-butt.png' });
+
+			postboxes.add_postbox_toggles(_wpcf7.screenId);
 
 			$('#contact-form-editor').tabs({
 				active: _wpcf7.activeTab,
