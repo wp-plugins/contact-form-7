df29935f97a81b2ee928f4b73ca8eac02a7c58c5
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-31 09:51:30 +0000

    restructure WPCF7_GetScorecard
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@1171259 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/getscorecard.php
+++ modules/getscorecard.php
@@ -7,11 +7,11 @@
 class WPCF7_GetScorecard extends WPCF7_Service {
 	const AUTH_EP = 'https://app.getscorecard.com/api/public/oauth';
 
-	public static function get_access_token() {
+	public function get_access_token() {
 		return get_transient( 'wpcf7_getscorecard_access_token' );
 	}
 
-	public static function delete_access_token() {
+	public function delete_access_token() {
 		return delete_transient( 'wpcf7_getscorecard_access_token' );
 	}
 
@@ -20,7 +20,7 @@ class WPCF7_GetScorecard extends WPCF7_Service {
 	}
 
 	public function is_connected() {
-		return (bool) self::get_access_token();
+		return (bool) $this->get_access_token();
 	}
 
 	public function is_active() {
@@ -40,7 +40,7 @@ class WPCF7_GetScorecard extends WPCF7_Service {
 	public function load( $action = '' ) {
 		if ( 'disconnect' == $action ) {
 			check_admin_referer( 'wpcf7-disconnect-getscorecard' );
-			self::delete_access_token();
+			$this->delete_access_token();
 
 			$redirect_to = add_query_arg(
 				array(
@@ -50,14 +50,6 @@ class WPCF7_GetScorecard extends WPCF7_Service {
 
 			wp_safe_redirect( $redirect_to );
 			exit();
-		}
-	}
-
-	public function display() {
-		if ( $this->is_connected() ) {
-			wpcf7_getscorecard_card_disconnect();
-		} else {
-			wpcf7_getscorecard_card_connect();
 		}
 	}
 
@@ -75,21 +67,28 @@ class WPCF7_GetScorecard extends WPCF7_Service {
 				esc_html( $message ) );
 		}
 	}
-}
 
-function wpcf7_getscorecard_card_disconnect() {
+	public function display() {
+		if ( $this->is_connected() ) {
+			$this->display_disconnect();
+		} else {
+			$this->display_connect();
+		}
+	}
+
+	private function display_disconnect() {
 ?>
 <form method="post" action="<?php echo esc_url( menu_page_url( 'wpcf7-integration', false ) ); ?>">
-<?php wp_nonce_field( 'wpcf7-disconnect-getscorecard' ); ?>
-<input type="hidden" name="service" value="getscorecard" />
-<input type="hidden" name="action" value="disconnect" />
+	<?php wp_nonce_field( 'wpcf7-disconnect-getscorecard' ); ?>
+	<input type="hidden" name="service" value="getscorecard" />
+	<input type="hidden" name="action" value="disconnect" />
 
-<p class="submit"><input type="submit" name="disconnect_getscorecard" class="button" value="<?php echo esc_attr( __( 'Disconnect from GetScorecard', 'contact-form-7' ) ); ?>" <?php echo "onclick=\"if (confirm('" . esc_js( __( "Are you sure you want to disconnect from GetScorecard?\n  'Cancel' to stop, 'OK' to disconnect.", 'contact-form-7' ) ) . "')) {return true;} return false;\""; ?> /></p>
+	<p class="submit"><input type="submit" name="disconnect_getscorecard" class="button" value="<?php echo esc_attr( __( 'Disconnect from GetScorecard', 'contact-form-7' ) ); ?>" <?php echo "onclick=\"if (confirm('" . esc_js( __( "Are you sure you want to disconnect from GetScorecard?\n  'Cancel' to stop, 'OK' to disconnect.", 'contact-form-7' ) ) . "')) {return true;} return false;\""; ?> /></p>
 </form>
 <?php
-}
+	}
 
-function wpcf7_getscorecard_card_connect() {
+	private function display_connect() {
 ?>
 <p><?php echo esc_html( __( "If you already have a GetScorecard account, sign in to GetScorecard.", 'contact-form-7' ) ); ?></p>
 
@@ -99,4 +98,6 @@ function wpcf7_getscorecard_card_connect() {
 
 <p><a href="https://app.getscorecard.com/register.php?registerType=gc_wp_plugin" class="button"><?php echo esc_html( __( 'Register', 'contact-form-7' ) ); ?></a></p>
 <?php
+	}
+
 }
