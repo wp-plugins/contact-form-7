7685ae603c956a1a8b6b5a3bc1321957da60d534
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-18 09:56:16 +0000

    Maybe using dashicons is smarter.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@1183270 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/edit-contact-form.php
+++ admin/edit-contact-form.php
@@ -167,7 +167,7 @@ if ( $post ) :
 
 <div id="postbox-container-2" class="postbox-container">
 <div id="contact-form-editor">
-<div class="keyboard-interaction"><?php echo esc_html( __( '&#x25C0; &#x25B6; keys switch panels', 'contact-form-7' ) ); ?></div>
+<div class="keyboard-interaction"><?php echo sprintf( esc_html( __( '%s keys switch panels', 'contact-form-7' ) ), '<span class="dashicons dashicons-leftright"></span>' ); ?></div>
 
 <?php
 
