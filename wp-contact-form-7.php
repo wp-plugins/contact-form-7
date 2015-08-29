43b1aea28eef31e6668290b0d046bcc5986d1ce3
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-29 02:27:54 +0000

    Update license.txt; add a note about reCAPTCHA icon license.
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7/trunk@1233515 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-contact-form-7.php
+++ wp-contact-form-7.php
@@ -10,23 +10,6 @@ Domain Path: /languages/
 Version: 4.2.2
 */
 
-/*  Copyright 2007-2015 Takayuki Miyoshi (email: takayukister at gmail.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
 define( 'WPCF7_VERSION', '4.2.2' );
 
 define( 'WPCF7_REQUIRED_WP_VERSION', '4.1' );
